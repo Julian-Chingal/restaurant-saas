4fa9ea89d5c3d13e730f22880193859a581dcda2
Julian Chingal <70417427+Julian-Chingal@users.noreply.github.com>
2026-01-20 16:18:43 -0500

    chore: update prisma seed configuration

--- apps/platform-service/prisma/seed.ts
+++ apps/platform-service/prisma/seed.ts
@@ -1,13 +1,16 @@
-import 'dotenv/config'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma/client'
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
+import { Pool } from 'pg';
 
-const connectionString = `${process.env.DATABASE_URL}`
-const pgAdapter = new PrismaPg(connectionString)
-const prisma = new PrismaClient({ adapter: pgAdapter })
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const pgAdapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter: pgAdapter });
 
 async function main() {
   if ((await prisma.tenantStatus.count()) !== 0) {
+    console.log('No ejecutar tenant status');
     return;
   } else {
     const tenantStatuses = [
@@ -33,6 +36,38 @@ async function main() {
 
     for (const status of tenantStatuses) {
       await prisma.tenantStatus.upsert({
+        where: { id: status.id },
+        update: {},
+        create: status,
+      });
+    }
+  }
+
+  if ((await prisma.subscriptionStatus.count()) !== 0) {
+    console.log('No Ejecutar Subscription Status');
+    return;
+  } else {
+    const subscriptionStatuses = [
+      {
+        id: 'ACTIVE',
+        description: 'Suscripción activa.',
+      },
+      {
+        id: 'PAST_DUE',
+        description: 'Fallo en el pago, reintentando',
+      },
+      {
+        id: 'UNPAID',
+        description: 'Suscripción terminada',
+      },
+      {
+        id: 'CANCELLED',
+        description: 'Suscripción que ha cancelado su suscripción.',
+      },
+    ];
+
+    for (const status of subscriptionStatuses) {
+      await prisma.subscriptionStatus.upsert({
         where: { id: status.id },
         update: {},
         create: status,
